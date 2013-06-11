09ea0af5136f70a728631fb98ceceec4f7552ee8
Michael Bosworth <mtbosworth@gmail.com>
2013-06-10 19:57:42 -0500

    stores data

---
+++ assets/js/cupcake.js
@@ -0,0 +1,19 @@
+
+var stores = [
+    {
+        "name" : "Carefree Patisserie",
+        "lat" : 41.572801,
+        "long" : -93.709304
+    }, 
+    {
+        "name" : "Scratch Cupcakes",
+        "lat" : 41.568164,
+        "long" : -93.804817
+    },
+    {
+        "name" : "Creme Cupcakes",
+        "lat"  : 41.543053,
+        "long"  :-93.661512
+    }
+
+];

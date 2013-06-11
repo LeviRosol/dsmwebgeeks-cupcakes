4f8d65d443ffe11eb90db7d886dd6f6fe02250cc
Michael Bosworth <mtbosworth@gmail.com>
2013-06-10 20:22:58 -0500

    alert

--- assets/js/cupcake.js
+++ assets/js/cupcake.js
@@ -20,8 +20,8 @@ var stores = [
 //$(document).ready(function(){
 
 var onSuccess = function(position) {
-    // alert('Latitude: '          + position.coords.latitude          + '\n' +
-    //       'Longitude: '         + position.coords.longitude         + '\n' +
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude );
     //       'Altitude: '          + position.coords.altitude          + '\n' +
     //       'Accuracy: '          + position.coords.accuracy          + '\n' +
     //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
